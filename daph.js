02c6009415c1425c432d5a84dc7f1af5e7bf4a47
daphtracy555-svg <daphtracy555@gmail.com>
2026-03-28 09:58:21 +0300

    Update daph.js

--- daph.js
+++ daph.js
@@ -16,11 +16,10 @@ app.use(express.urlencoded({ extended: true }));
 const db = mysql.createConnection({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
-  password: process.env.DB_PASS,
+  password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  port: process.env.DB_PORT   // port number
+  port: process.env.DB_PORT ? Number(process.env.DB_PORT) : 3306
 });
-
 db.connect(err => {
   if (err) {
     console.error("DB connection failed:", err);
