ea3573927166df0449bb4286a91f1ab56a771868
daphtracy555-svg <daphtracy555@gmail.com>
2026-03-28 10:13:38 +0300

    Update daph.js

--- daph.js
+++ daph.js
@@ -14,11 +14,10 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.urlencoded({ extended: true }));
 // Database connection
 const db = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  port: process.env.DB_PORT ? Number(process.env.DB_PORT) : 3306
+  uri: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 db.connect(err => {
   if (err) {
